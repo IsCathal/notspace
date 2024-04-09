7af9649ffc2d3c022ac59d4474b010684a95be23
IsCathal <mccabemcathal@gmail.com>
2024-04-09 08:46:33 +0100

    got a nav bar going

--- app/NavMenu.tsx
+++ app/NavMenu.tsx
@@ -4,25 +4,22 @@ import styles from "./NavMenu.module.css";
 
 export default function NavMenu() {
     return (    
-
-
-            //href '/' is the root of the website
-            // it will take you to the root of the website
-
+             //href '/' is the root of the website
+            // it will take you to the root of the website  
         <nav className={styles.nav}>
-            <link href={'/'}>
+            <Link href={'/'}>
             
-            <Image
-                src = "/logo.png"
-                width = {100}
-                height = {100}
-                alt = "logo"
-                />
-            </link>
+                 <Image
+                       src = "/logo.svg"
+                          width = {216}
+                          height = {30}
+                          alt = "logo"
+                  />
+            </Link>
 
             <ul className={styles.links}>    
                 <li>
-                    <Link href={'/about'}>About</Link>
+                <Link href={'/about'}>About</Link>
                 </li>
                 <li>
                 <Link href={'/blog'}>Blog</Link>
